3c1fb3562a37661d0584d0a5a27264f3b690247b
Ozgur Ozer <ozgr@live.com>
2020-11-29 14:05:57 +0300

    Update render file name

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const port = 1230
 
 const defaults = require.main.require('./defaults')
 
@@ -11,12 +10,13 @@ app.use(express.static(path.join(__dirname, '..', '..', 'dist')))
 
 const getTimeForConsole = () => new Date(Date.now()).toLocaleString() + ':'
 
+const port = process.env.PORT || defaults.site.port
 app.listen(port, () => {
-  console.log(getTimeForConsole(), `${defaults.site.name}: http://localhost:${defaults.site.port}`)
+  console.log(getTimeForConsole(), `${defaults.site.name}: http://localhost:${port}`)
 })
 
 app.get('*', (req, res) => {
-  res.render('App', {
+  res.render('app', {
     defaults: {
       siteName: defaults.site.name
     }
