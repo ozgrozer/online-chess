73e75253e4fad4ee722fdc2a21af2f695bab1554
Glitch (online-chess) <none>
2020-11-29 11:09:10 +0000

    Before import of ozgrozer/online-chess without specified path ./src/backend/server.js:5277/1278

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const port = 1230
 
 const defaults = require.main.require('./defaults')
 
@@ -11,10 +10,15 @@ app.use(express.static(path.join(__dirname, '..', '..', 'dist')))
 
 const getTimeForConsole = () => new Date(Date.now()).toLocaleString() + ':'
 
-app.listen(process.env.PORT, () => {
-  console.log(getTimeForConsole(), `${defaults.site.name}: http://localhost:${defaults.site.port}`)
+const port = process.env.PORT || defaults.site.port
+app.listen(port, () => {
+  console.log(getTimeForConsole(), `${defaults.site.name}: http://localhost:${port}`)
 })
 
 app.get('*', (req, res) => {
-  res.send('asd')
+  res.render('app', {
+    defaults: {
+      siteName: defaults.site.name
+    }
+  })
 })
