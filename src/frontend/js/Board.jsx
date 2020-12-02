6217529b757d154b40044a904aa669753a10d537
Ozgur Ozer <ozgr@live.com>
2020-12-03 02:35:54 +0300

    Add fen coordinates

--- src/frontend/js/Board.jsx
+++ src/frontend/js/Board.jsx
@@ -30,7 +30,7 @@ const defaults = {
   }
 }
 
-const defaultCoordinate = [
+const defaultCoordinates = [
   ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
   ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
   ['.', '.', '.', '.', '.', '.', '.', '.'],
@@ -139,6 +139,20 @@ const calculatePossibleMoves = props => {
       const nextRank = color === 'white' ? rank + 2 : rank - 2
       possibleMoves.push({ rank: nextRank, file })
     }
+  } else if (name === 'rook') {
+    for (let col = 0; col < defaults.board.cols; col++) {
+      const newFile = (rank - 1) !== col ? files[col] : ''
+      if (newFile) {
+        possibleMoves.push({ rank, file: newFile })
+      }
+    }
+
+    for (let row = 0; row < defaults.board.rows; row++) {
+      const newRank = (rank) !== row ? row : ''
+      if (newRank) {
+        possibleMoves.push({ rank: newRank, file })
+      }
+    }
   }
 
   return possibleMoves
@@ -227,52 +241,142 @@ const ShowPossibleMoves = props => {
   )
 }
 
+const arrayToFen = props => {
+  const { arrayCoordinates } = props
+
+  const combineFiles = []
+
+  for (const key in arrayCoordinates) {
+    const defaultCoordinateRanks = arrayCoordinates[key]
+    const combineRanks = []
+
+    for (const key in defaultCoordinateRanks) {
+      const defaultCoordinateRank = defaultCoordinateRanks[key]
+      combineRanks.push(defaultCoordinateRank)
+    }
+
+    const combineRanksString = combineRanks.join('')
+      .replace(/\.\.\.\.\.\.\.\./g, 8)
+      .replace(/\.\.\.\.\.\.\./g, 7)
+      .replace(/\.\.\.\.\.\./g, 6)
+      .replace(/\.\.\.\.\./g, 5)
+      .replace(/\.\.\.\./g, 4)
+      .replace(/\.\.\./g, 3)
+      .replace(/\.\./g, 2)
+      .replace(/\./g, 1)
+
+    combineFiles.push(combineRanksString)
+  }
+
+  return combineFiles.join('/')
+}
+
+const fenToArray = props => {
+  const { fenCoordinates } = props
+
+  const replaceNumbers = fenCoordinates
+    .replace(/8/g, '........')
+    .replace(/7/g, '.......')
+    .replace(/6/g, '......')
+    .replace(/5/g, '.....')
+    .replace(/4/g, '....')
+    .replace(/3/g, '...')
+    .replace(/2/g, '..')
+    .replace(/1/g, '.')
+
+  const rows = replaceNumbers.split('/')
+
+  const array = []
+  for (const key in rows) {
+    const row = rows[key]
+    const newRow = []
+    for (const key in row) {
+      const item = row[key]
+      newRow.push(item)
+    }
+    array.push(newRow)
+  }
+
+  return array
+}
+
 const Board = () => {
-  const [pieces, setPieces] = useState(defaultCoordinate)
+  const [pieces, setPieces] = useState(defaultCoordinates)
   const [possibleMoves, setPossibleMoves] = useState([])
   const [selectedPiece, setSelectedPiece] = useState({ color: '', name: '', coordinates: { file: '', rank: '' } })
+  const selectedPieceReadable = selectedPiece.color
+    ? `${selectedPiece.color} ${selectedPiece.name} ${selectedPiece.coordinates.file}${selectedPiece.coordinates.rank}`
+    : 'waiting...'
 
   const boardStyle = {
     width: defaults.square.size * defaults.board.cols,
     height: defaults.square.size * defaults.board.rows
   }
 
-  const defaultCoordinateForTextarea = JSON.stringify(pieces)
+  const arrayToFen_ = arrayToFen({ arrayCoordinates: pieces })
+  const [fenCoordinates, setFenCoordinates] = useState(arrayToFen_)
+  const fenCoordinatesOnChange = event => {
+    const fenCoordinates = event.target.value
+    setFenCoordinates(fenCoordinates)
+
+    const fenToArray_ = fenToArray({ fenCoordinates })
+    setPieces(fenToArray_)
+  }
+
+  const arrayCoordinates = JSON.stringify(pieces)
     .replace(/\[\[/g, '[\n  [')
     .replace(/\]\]/g, ']\n]')
     .replace(/,\[/g, ',\n  [')
-  const textareaOnChange = event => {
-    setPieces(JSON.parse(event.target.value))
+  const arrayCoordinatesOnChange = event => {
+    const pieces = JSON.parse(event.target.value)
+    setPieces(pieces)
+
+    const arrayToFen_ = arrayToFen({ arrayCoordinates: pieces })
+    setFenCoordinates(arrayToFen_)
   }
 
   return (
     <React.Fragment>
-      <div id='selectedPiece'>
-        Selected piece: <b>{selectedPiece.color} {selectedPiece.name} {selectedPiece.coordinates.file}{selectedPiece.coordinates.rank}</b>
-      </div>
+      <div className='boardWrapper'>
+        <div className='boardLeft'>
+          <div
+            className='board'
+            style={boardStyle}
+          >
+            <MakeSquares />
+            <PlacePieces
+              pieces={pieces}
+              setPieces={setPieces}
+              selectedPiece={selectedPiece}
+              setPossibleMoves={setPossibleMoves}
+              setSelectedPiece={setSelectedPiece}
+            />
+            <ShowPossibleMoves possibleMoves={possibleMoves} />
+          </div>
+        </div>
 
-      <div
-        id='board'
-        style={boardStyle}
-      >
-        <MakeSquares />
-        <PlacePieces
-          pieces={pieces}
-          setPieces={setPieces}
-          selectedPiece={selectedPiece}
-          setPossibleMoves={setPossibleMoves}
-          setSelectedPiece={setSelectedPiece}
-        />
-        <ShowPossibleMoves possibleMoves={possibleMoves} />
-      </div>
+        <div className='boardRight'>
+          <div className='selectedPiece'>
+            Selected piece: <b>{selectedPieceReadable}</b>
+          </div>
 
-      <textarea
-        rows='10'
-        id='defaultCoordinate'
-        onChange={textareaOnChange}
-        value={defaultCoordinateForTextarea}
-        style={{ width: boardStyle.width }}
-      />
+          <textarea
+            rows='2'
+            value={fenCoordinates}
+            className='defaultCoordinates'
+            onChange={fenCoordinatesOnChange}
+            style={{ width: boardStyle.width }}
+          />
+
+          <textarea
+            rows='10'
+            value={arrayCoordinates}
+            className='defaultCoordinates'
+            onChange={arrayCoordinatesOnChange}
+            style={{ width: boardStyle.width }}
+          />
+        </div>
+      </div>
     </React.Fragment>
   )
 }
